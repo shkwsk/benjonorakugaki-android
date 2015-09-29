971e3c364f6aa67fd538e1fd1e40e5c0c97abcd8
shkwsk <shkwsk@icloud.com>
2015-09-30 02:11:36 +0900

    New: QRCodeReading by ZXing. Add: reader range is narrowed by rectView.

--- app/src/main/java/com/example/shkwsk/myapp01/QRReaderActivity.java
+++ app/src/main/java/com/example/shkwsk/myapp01/QRReaderActivity.java
@@ -1,39 +1,181 @@
 package com.example.shkwsk.myapp01;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-import com.example.shkwsk.myapp01.R;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.Reader;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+
+import java.io.IOException;
 
 public class QRReaderActivity extends AppCompatActivity {
+    private static final String TAG = "CameraSample";
+
+    private int CAMERA_ID = 0; // for Nexus7
+
+    private Camera camera;
+    private RelativeLayout relativeLayout;
+    private SurfaceView surfaceView;
+    private View rectView;
+    private CameraListener cameraListener = new CameraListener();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qrreader);
+        System.out.println("QRReaderActivity");
+        relativeLayout = new RelativeLayout(this);
+        surfaceView = new SurfaceView(this);
+
+        rectView = new View(this) {
+            @Override
+            protected void onDraw(Canvas canvas) {
+                Paint bgpaint = new Paint();
+                bgpaint.setColor(0x99ffffff);
+                canvas.drawRect(0, 0, surfaceView.getWidth(), surfaceView.getHeight(), bgpaint);
+
+                Paint paint = new Paint();
+                paint.setColor(Color.TRANSPARENT);
+                paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+                float widthQuarter = surfaceView.getWidth() / 4;
+                float heightQuarter = surfaceView.getHeight() / 4;
+                canvas.drawRect(widthQuarter , heightQuarter, widthQuarter * 3, heightQuarter * 3, paint);
+            }
+        };
+
+        SurfaceHolder holder = surfaceView.getHolder();
+        holder.addCallback(cameraListener);
+        relativeLayout.addView(surfaceView);
+        relativeLayout.addView(rectView);
+        setContentView(relativeLayout);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_qrreader, menu);
-        return true;
+    private int getOrientation() {
+        return getResources().getConfiguration().orientation;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class CameraListener implements
+            SurfaceHolder.Callback,
+            AutoFocusCallback,
+            Camera.PictureCallback,
+            Camera.PreviewCallback
+    {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            camera = Camera.open(CAMERA_ID);
+            try {
+                camera.setPreviewDisplay(holder);
+            } catch (IOException e) {
+                Log.e(TAG, e.toString(), e);
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format,
+                                   int width, int height) {
+
+            // カメラのプレビューサイズをViewに設定
+            Camera.Parameters parameters = camera.getParameters();
+            Camera.Size size = parameters.getSupportedPreviewSizes().get(0); // 0=最大サイズ
+            parameters.setPreviewSize(size.width, size.height);
+            camera.setParameters(parameters);
+
+            // 画面回転補正。
+            ViewGroup.LayoutParams layoutParams = surfaceView.getLayoutParams();
+            if (getOrientation() == Configuration.ORIENTATION_PORTRAIT) {
+                camera.setDisplayOrientation(90);
+                layoutParams.width = size.height;
+                layoutParams.height = size.width;
+            } else {
+                camera.setDisplayOrientation(0);
+                layoutParams.width = size.width;
+                layoutParams.height = size.height;
+            }
+            surfaceView.setLayoutParams(layoutParams);
+
+            // オートフォーカス設定。
+            camera.autoFocus(cameraListener);
+
+            camera.startPreview();
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            camera.autoFocus(null);
+            camera.setPreviewCallback(null);
+            camera.release();
+            camera = null;
+        }
+
+        @Override
+        public void onAutoFocus(boolean success, Camera camera) {
+            if (success) {
+                Log.d(TAG, "focus");
+                // プレビューのデータ取得。
+                camera.setPreviewCallback(cameraListener);
+                // フルサイズ画像はTODO
+                //camera.takePicture(null,null,cameraListener);
+            }
+        }
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // フルサイズ画像もやることは同じ。
+            onPreviewFrame(data, camera);
+        }
+
+        @Override
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            // 処理は1回なのでコールバック取り消し
+            camera.setPreviewCallback(null);
+
+            // 基礎データ取得
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(CAMERA_ID, info);
+            int w = camera.getParameters().getPreviewSize().width;
+            int h = camera.getParameters().getPreviewSize().height;
+            boolean isMirror = (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT);
+
+            // プレビュー画像の型変換
+            PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(
+                    data, w, h, w/4, h/4, w/2, h/2, isMirror);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+            // QRコード読み込み。
+            Reader reader = new MultiFormatReader();
+            try {
+                Log.d(TAG, "decode");
+                Result result = reader.decode(bitmap);
+                String text = result.getText();
+
+                Toast.makeText(QRReaderActivity.this, text, Toast.LENGTH_LONG).show();
+                Log.i(TAG, "result:" + text);
+                camera.stopPreview();
+                camera.autoFocus(null);
+            } catch (Exception e) {
+                // QRコード認識失敗でも例外発生する。
+                Log.d(TAG, "decode-fail:" + e.toString());
+                camera.autoFocus(cameraListener);
+            }
+        }
     }
 }
